eac6c1f8959b8816230bb1b1f128cc99e5b95c28
Roel van der Heijden <Roel.Heijden@hotmail.com>
2022-05-05 19:10:29 +0200

    Backup 05/05/2022

---
+++ PokemonML/model/test.py
@@ -0,0 +1,7 @@
+import torch
+
+
+accounts = ["ketchupplant", "planta roel", "bring back gen 4", "paul blart mal cop", "planta real",
+            "twitter drama", "cluelessvgc", "sendhelpvgc", "music television", "heart monitor", "i'm out of alts"]
+
+
