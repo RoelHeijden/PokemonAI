1a83252266f64f532405508fbf5e1cc6b3078214
Roel van der Heijden <Roel.Heijden@hotmail.com>
2022-05-19 19:31:29 +0200

    major restructuring of dead functions/files and folders

--- testing.py
+++ testing.py
@@ -8,8 +8,13 @@ import time
 import re
 import shutil
 
+path = 'C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/processed-ou-incomplete/all_rated_1200+/training_states/'
 
+name = 'gen8ou-100012-state-22.json'
 
+file = json.load(open(os.path.join(path, name), 'r'))
+
+print(json.dumps(file, indent=3))
 
 
 
