df0ba7026142c657bf1503d56217d9d13b234c9a
Roel van der Heijden <Roel.Heijden@hotmail.com>
2022-05-20 23:30:32 +0200

    Restructured, removed the (no longer) necessary functions. extracting states now stored in batch .jsonl files

--- PokemonML/data_processing/main.py
+++ PokemonML/data_processing/main.py
@@ -1,4 +1,3 @@
-
 import json
 import os
 import numpy as np
@@ -9,33 +8,39 @@ import math
 from game_parser.game_log import GameLog
 
 
+"""
+
+BUGS:
+    - normalize_name() removes numbers (e.g. Porygon2 -> porygon)
+    - Gravity, Magic room and Wonder room(?) override terrains in Battle.field
+
+NOTEWORTHY:
+    - states consists of moments in a turn where one of the sides has a fainted Pokemon
+    - Zoroark/Illusion is not implemented yet
+    
+"""
+
+
 def main():
-    # mode = 'read from folder'
-    # mode = 'pre-process data'
-    mode = 'extract training states'
-    # mode = 'inspect a log'
+    # mode = 'parse games'
+    mode = 'create training state batches'
+    # mode = 'inspect a game'
 
-    battle_id = 1021737
+    inspect_battle_id = 1021737
 
-    if mode == 'read from folder':
-        path_in = "C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/pre-processed-ou-dec2019-feb2022/anonymized-ou-incomplete/all_rated_1200+"
+    if mode == 'parse games':
+        path_in = "C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/raw-ou-incomplete"
         path_out = 'C:/Users/RoelH/Documents//Uni/Bachelor thesis/data/processed-ou-incomplete/all_rated_1200+/training_games'
-        parse_all(path_in, path_out)
+        parse_all(path_in, path_out, min_rating=1200)
 
-    if mode == 'pre-process data':
-        path_in = "C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/anonymized-ou-Dec2019-Feb2020/anonymized-ou-incomplete"
-        path_out = "C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/pre-processed-ou-dec2019-feb2022/anonymized-ou-incomplete"
-        pre_pre_processing(path_in, path_out)
-
-    if mode == 'extract training states':
+    if mode == 'create training state batches':
         path_in = 'C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/processed-ou-incomplete/all_rated_1200+/training_games'
-        path_out = 'C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/processed-ou-incomplete/all_rated_1200+/training_states'
-        extract_training_states(path_in, path_out)
+        path_out = 'C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/processed-ou-incomplete/all_rated_1200+/training_states_large'
+        extract_training_states(path_in, path_out, file_name='ou_game_states', batch_size=10000, min_game_length=3)
 
-    if mode == 'inspect a log':
-        path_in = "C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/pre-processed-ou-dec2019-feb2022/anonymized-ou-incomplete/all_rated_1200+"
-
-        file = os.path.join(path_in, 'battle-gen8ou-' + str(battle_id) + '.log.json')
+    if mode == 'inspect a game':
+        path_in = "C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/raw-ou-incomplete"
+        file = os.path.join(path_in, 'battle-gen8ou-' + str(inspect_battle_id) + '.log.json')
         f = open(file, 'r')
 
         info = None
@@ -51,11 +56,12 @@ def main():
             print(mon)
 
 
-def parse_all(folder_path, save_path):
+def parse_all(folder_path, save_path, min_rating=1200):
     """ Parses the game states, writes new json files to folders sorted on average rating """
 
-    print("creating file names")
+    print("collecting file names\n")
 
+    # collect files
     files = sorted(
         [
             (
@@ -81,73 +87,18 @@ def parse_all(folder_path, save_path):
         if ignore_list.get(str(battle_id)):
             continue
 
+        # open raw battle json
         with open(path_in, "r", encoding='utf-8') as f_in:
             for line in f_in:
 
                 info = ujson.loads(line)
 
-                # only check games post dynamax ban, which I think was on december 17
+                # only check games post dynamax ban, which was on december 17th. Checking 18th to be sure
                 date = info['timestamp'].split(" ")
-                if date[1] == 'Dec' and int(date[2]) < 18:
+                if date[1] == 'Dec' and int(date[2]) <= 18:
                     continue
 
-                # extract game states
-                game = GameLog(info, battle_id)
-                parsed_replay = game.parse_replay()
-
-                # write parsed file
-                path_out = os.path.join(save_path, file_out)
-                f_out = open(path_out, "w+")
-                f_out.write(ujson.dumps(parsed_replay))
-
-                match_count += 1
-
-                if match_count % 10000 == 0:
-                    print(f'{match_count} games processed, {round(time.time() - tic)}s')
-
-    toc = time.time()
-
-    print("Match count: {}".format(match_count))
-    print("Total time: {:.2f}s".format(toc - tic))
-
-
-def pre_pre_processing(folder_path, save_path):
-    """ does some preprocessing:
-        - removes games played before december 18th (pre dmax ban)
-        - sort games to folders based on average rating
-        - changes 'null' variables
-    """
-
-    print("creating file names")
-
-    files = sorted(
-        [
-            (
-                os.path.join(folder_path, file_name),
-                file_name,
-                int(file_name.split('-')[2].strip('.log.json'))
-            )
-            for file_name in os.listdir(folder_path)
-        ]
-    )
-
-    print(f'{len(files)} files found\n')
-
-    match_count = 0
-    tic = time.time()
-
-    for path_in, file_out, battle_id in files:
-        with open(path_in, "r", encoding='utf-8') as f_in:
-            for line in f_in:
-
-                info = ujson.loads(line)
-
-                # only check games post dynamax ban, which I think was on december 17
-                date = info['timestamp'].split(" ")
-                if date[1] == 'Dec' and int(date[2]) < 18:
-                    continue
-
-                # get rating and edit null variables
+                # replace the 'null' values
                 rated_battle, p1_rating, p2_rating = get_player_ratings(info['inputLog'], battle_id)
                 info['rated_battle'] = rated_battle
                 info['p1rating'] = p1_rating
@@ -155,40 +106,23 @@ def pre_pre_processing(folder_path, save_path):
                 info['roomid'] = battle_id
                 info['average_rating'] = int((p1_rating + p2_rating) / 2)
 
-                # sort based on average rating
-                if info.get('rated_battle'):
-                    rating = info['average_rating']
-                    if 1000 <= rating <= 1199:
-                        folder = "rated_1000_1199"
-                    elif 1200 <= rating <= 1399:
-                        folder = "rated_1200_1399"
-                    elif 1400 <= rating <= 1599:
-                        folder = "rated_1400_1599"
-                    elif 1600 <= rating <= 1799:
-                        folder = "rated_1600_1799"
-                    elif 1800 <= rating:
-                        folder = "rated_1800+"
-                    else:
-                        raise ValueError(
-                            "Rated battle rating {} not between 999 and inf -- battle id: {}".format(rating, battle_id)
-                        )
-                else:
-                    folder = "unrated"
+                # skip if rating is too low
+                if info['average_rating'] < min_rating:
+                    continue
 
-                # create directory if it doesn't exist
-                folder_out = os.path.join(save_path, folder)
-                if not os.path.exists(folder_out):
-                    os.mkdir(folder_out)
+                # parse game and extract game states
+                game = GameLog(info, battle_id)
+                parsed_game = game.parse_replay()
 
-                # write new file
-                path_out = os.path.join(folder_out, file_out)
+                # write parsed game_states file
+                path_out = os.path.join(save_path, file_out)
                 f_out = open(path_out, "w+")
-                f_out.write(ujson.dumps(info))
+                f_out.write(ujson.dumps(parsed_game))
 
                 match_count += 1
 
                 if match_count % 10000 == 0:
-                    print(f'{match_count} games processed, {round(time.time() - tic)} seconds passed')
+                    print(f'{match_count} games processed, {round(time.time() - tic)}s')
 
     toc = time.time()
 
@@ -229,10 +163,14 @@ def get_player_ratings(game_input_log, battle_id):
     return is_rated_battle, p1_rating, p2_rating
 
 
-def extract_training_states(path_in, path_out, min_game_length=3):
-    """ converts each game into one or two number-converted game states """
+def extract_training_states(path_in, path_out, file_name='ou_game_states', batch_size=10000, min_game_length=3):
+    """
+    - extracts two game states from each game-states file
+    - switches player's POV for one of the states
+    - writes as batches to .jsonl files
+    """
 
-    print("starting\n")
+    print("collecting file names\n")
 
     # collect file names
     files = sorted([os.path.join(path_in, file_name)
@@ -241,11 +179,17 @@ def extract_training_states(path_in, path_out, min_game_length=3):
     print(f'{len(files)} files found\n')
 
     start_time = time.time()
-    n_states = 0
+
     n_games = 0
+    n_states = 0
+    n_written = 0
 
     games_too_short = {}
     ignore_list = json.load(open('games_to_ignore.json', 'r'))
+
+    # initialize first out file
+    f_out = open(os.path.join(path_out, file_name + '0.jsonl'), 'w')
+    n_written += 1
 
     # open each game_states file
     for f in files:
@@ -260,34 +204,42 @@ def extract_training_states(path_in, path_out, min_game_length=3):
 
             # skip game if game doesn't last long enough
             if len(all_states) < min_game_length:
-                games_too_short[f] = len(all_states)
+                games_too_short[room_id] = len(all_states)
                 continue
 
             # select two random states to write
-            states, state_nums = pick_random_states(all_states, min_turns_apart=math.floor(min_game_length/3))
-
-            # write each extracted state to separate file
-            for i, (s, n) in enumerate(zip(states, state_nums)):
-
-                file_name = os.path.join(path_out, 'gen8ou-' + str(room_id) + '-state-' + str(n) + '.json')
-                with open(file_name, 'w') as f_out:
-                    f_out.write(ujson.dumps(s))
-
-                n_states += 1
+            states = pick_random_states(all_states, min_turns_apart=math.floor(min_game_length/3))
 
             n_games += 1
 
+            # write each extracted state to batch file
+            for s in states:
+                f_out.write(ujson.dumps(s))
+                f_out.write('\n')
+
+                n_states += 1
+
+            # close previous, and initialize new batch file
+            if n_states % batch_size == 0:
+                f_out.close()
+                file = file_name + str(int(n_states / batch_size)) + '.jsonl'
+                f_out = open(os.path.join(path_out, file), 'w')
+
+                n_written += 1
+
+            # keeping you updated
             if n_games % 2000 == 0:
                 print(f'{n_games} games opened')
                 print(f'{n_states} states extracted')
+                print(f'{n_written} batch files created')
                 print(f'runtime: {round(time.time() - start_time, 1)}s\n')
 
-    print("finished\n")
-
-    print(f"{len(games_too_short)} games skipped because they lasted shorter than {min_game_length} turns")
     print(ujson.dumps(games_too_short))
+    print(f"{len(games_too_short)} games skipped because they lasted shorter than {min_game_length} turns")
 
-    print(f'\n{n_games} games opened, {n_states} states extracted')
+    print(f'{n_games} games opened')
+    print(f'{n_states} states extracted')
+    print(f'{n_written} batch files created')
     print(f'runtime: {round(time.time() - start_time, 1)}s\n')
 
 
@@ -302,7 +254,7 @@ def pick_random_states(all_states, min_turns_apart=1):
     state1 = all_states[state_idx1]
     state2 = all_states[state_idx2]
 
-    return [state1, reverse_pov(state2)], [state_idx1, state_idx2]
+    return [state1, reverse_pov(state2)]
 
 
 def reverse_pov(state):
