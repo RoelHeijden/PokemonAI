5c57776e67e147216716193cc363b03a4ad4df23
Roel van der Heijden <Roel.Heijden@hotmail.com>
2022-05-20 23:30:38 +0200

    tests

--- testing.py
+++ testing.py
@@ -8,18 +8,28 @@ import time
 import re
 import shutil
 
-path = 'C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/processed-ou-incomplete/all_rated_1200+/training_states/'
 
-name = 'gen8ou-100012-state-22.json'
+path_in = 'C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/processed-ou-incomplete/all_rated_1200+/training_states_large/'
 
-file = json.load(open(os.path.join(path, name), 'r'))
+files = [
+    os.path.join(path_in, file_name)
+    for file_name in os.listdir(path_in)
+]
+print(len(files), "files found")
 
-print(json.dumps(file, indent=3))
+i = 0
+states = []
+for f in files:
+    with open(f, 'r') as f_in:
+        for line in f_in:
+            state = ujson.loads(line)
+            states.append(state)
+            i += 1
 
+            if i % 1000 == 0:
+                print(f'{i} states opened')
 
-
-
-
+print(json.dumps(states[17001], indent=3))
 
 
 
