c0d20156deedb2f9202a4e811d4351471dba14ca
Roel van der Heijden <Roel.Heijden@hotmail.com>
2022-05-22 11:09:08 +0200

    tests

--- testing.py
+++ testing.py
@@ -7,38 +7,22 @@ import csv
 import time
 import re
 import shutil
+import torch
+
+s = time.time()
+
+for i in range(100000):
+    item = torch.tensor([[1, 2, 3, 4, 5, 6],
+                         [1, 2, 3, 4, 5, 6]])
+
+    species = torch.tensor([[1, 2, 3, 4, 5, 6],
+                            [1, 2, 3, 4, 5, 6]])
+
+    total = torch.stack((item, species))
+    # total = torch.cat((item, species))
 
 
-path_in = 'C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/processed-ou-incomplete/all_rated_1200+/training_states_large/'
-
-files = [
-    os.path.join(path_in, file_name)
-    for file_name in os.listdir(path_in)
-]
-print(len(files), "files found")
-
-i = 0
-states = []
-for f in files:
-    with open(f, 'r') as f_in:
-        for line in f_in:
-            state = ujson.loads(line)
-            states.append(state)
-            i += 1
-
-            if i % 1000 == 0:
-                print(f'{i} states opened')
-
-print(json.dumps(states[17001], indent=3))
-
-
-
-
-
-
-
-
-
+print(time.time() - s)
 
 
 
