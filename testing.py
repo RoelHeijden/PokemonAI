5e0a09ea8dbad7b741efcb49c97f61ae98921030
Roel van der Heijden <Roel.Heijden@hotmail.com>
2022-05-24 19:28:26 +0200

    tests

--- testing.py
+++ testing.py
@@ -1,3 +1,5 @@
+import random
+
 import pandas as pd
 import numpy as np
 import os
@@ -8,25 +10,29 @@ import time
 import re
 import shutil
 import torch
-
-s = time.time()
-
-for i in range(100000):
-    item = torch.tensor([[1, 2, 3, 4, 5, 6],
-                         [1, 2, 3, 4, 5, 6]])
-
-    species = torch.tensor([[1, 2, 3, 4, 5, 6],
-                            [1, 2, 3, 4, 5, 6]])
-
-    total = torch.stack((item, species))
-    # total = torch.cat((item, species))
+import torch.nn as nn
 
 
-print(time.time() - s)
+def inspect_states():
+    path = 'C:/Users/RoelH/Documents//Uni/Bachelor thesis/data/processed-ou-incomplete/all_rated_1200+/test_games'
 
+    files = [os.path.join(path, file_name)
+             for file_name in os.listdir(path)]
 
+    print(f'{len(files)} files found')
 
+    random.shuffle(files)
 
+    for file in files[:10000]:
+        with open(file, 'r') as f_in:
+            states = json.load(f_in)
 
+            for i, s in enumerate(states):
+                if s['p1']['wish']['countdown'] > 1 or s['p2']['wish']['countdown'] > 1:
+                    print(json.dumps(s, indent=3))
+                    break
+            else:
+                continue
+            break
 
 
