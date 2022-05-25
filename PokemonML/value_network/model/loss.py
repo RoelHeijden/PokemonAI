e5726213f208a5af21b8088eb9bf52dcddb2ef7a
Roel van der Heijden <Roel.Heijden@hotmail.com>
2022-05-26 00:54:46 +0200

    added L2 option

--- PokemonML/value_network/model/loss.py
+++ PokemonML/value_network/model/loss.py
@@ -3,10 +3,13 @@ import torch.nn as nn
 
 
 class Loss(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, loss='BCE'):
         super().__init__()
 
-        self.bce_loss = nn.BCELoss()
+        if loss == 'L1':
+            self.loss_function = nn.L1Loss()
+        else:
+            self.loss_function = nn.BCELoss()
 
     def forward(self, x, result):
-        return self.bce_loss(torch.squeeze(x), result)
+        return self.loss_function(torch.squeeze(x), result)
