d7b130455c5c85ac79f3d9c397bc0c503f712291
Roel van der Heijden <Roel.Heijden@hotmail.com>
2022-05-25 22:39:32 +0200

    settings change

--- PokemonML/value_network/model/encoder.py
+++ PokemonML/value_network/model/encoder.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from typing import Dict
 
 from data.categories import (
     SPECIES,
@@ -15,17 +16,18 @@ class Encoder(nn.Module):
         self.flatten = nn.Flatten(start_dim=1)
 
         self.species_embedding = nn.Embedding(len(SPECIES) + 1, 64, padding_idx=0)
-        self.move_embedding = nn.Embedding(len(MOVES) + 1, 32, padding_idx=0)
-        self.item_embedding = nn.Embedding(len(ITEMS) + 1, 16, padding_idx=0)
-        self.ability_embedding = nn.Embedding(len(ABILITIES) + 1, 16, padding_idx=0)
+        self.move_embedding = nn.Embedding(len(MOVES) + 1, 128, padding_idx=0)
+        self.item_embedding = nn.Embedding(len(ITEMS) + 1, 32, padding_idx=0)
+        self.ability_embedding = nn.Embedding(len(ABILITIES) + 1, 64, padding_idx=0)
 
-    def _concat_fields(self, fields):
-        return fields
-
-    def _concat_sides(self, sides):
-        return self.flatten(sides)
-
-    def _concat_pokemon(self, pokemon):
+    def _concat_pokemon(self, pokemon: Dict[str, torch.tensor]):
+        """
+        returns:
+            for each player:
+                for each pokemon:
+                    'embeddings': concatenated pokemon embeddings
+                    'attributes': concatenated pokemon attributes
+        """
         species = self.species_embedding(pokemon["species"])
         moves = self.move_embedding(pokemon["moves"])
         items = self.item_embedding(pokemon["items"])
@@ -38,9 +40,10 @@ class Encoder(nn.Module):
                         species[:, j][:, i],
                         items[:, j][:, i],
                         abilities[:, j][:, i],
-                        pokemon['pokemon_attributes'][:, j][:, i],
                         self.flatten(moves[:, j][:, i]),
-                        self.flatten(pokemon['move_attributes'][:, j][:, i])
+                        # torch.mean(moves[:, j][:, i], dim=1),
+                        self.flatten(pokemon['move_attributes'][:, j][:, i]),
+                        pokemon['pokemon_attributes'][:, j][:, i],
                     ),
                     dim=1
                 )
@@ -50,31 +53,6 @@ class Encoder(nn.Module):
         ]
 
     def forward(self, fields, sides, pokemon):
-        fields = self._concat_fields(fields)
-        sides = self._concat_sides(sides)
-        pokemon = self._concat_pokemon(pokemon)
-
-        # temporary test
-        return torch.cat(
-            (
-                fields,
-                sides,
-                pokemon[0][0],
-                pokemon[0][1],
-                pokemon[0][2],
-                pokemon[0][3],
-                pokemon[0][4],
-                pokemon[0][5],
-                pokemon[1][0],
-                pokemon[1][1],
-                pokemon[1][2],
-                pokemon[1][3],
-                pokemon[1][4],
-                pokemon[1][5],
-            ),
-            dim=1
-        )
-
-
+        return fields, sides, self._concat_pokemon(pokemon)
 
 
