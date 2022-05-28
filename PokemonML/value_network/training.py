c414833883439db7da8fd6f234ca4be5925268f2
Roel van der Heijden <Roel.Heijden@hotmail.com>
2022-05-28 14:52:54 +0200

    settings/notes, data folder passable

--- PokemonML/value_network/training.py
+++ PokemonML/value_network/training.py
@@ -10,73 +10,64 @@ from data.transformer import StateTransformer
 
 """
 BEST: 
-ValueNet2 1500+, LR=2e-4, gamma=0.9, 128/16/16/16, 192 drop, -, 1024, 512, 256 -- epoch: 20, acc: 0.755
+1250+, LR=2e-4, gamma=0.9, 128/16/16/16, pokemon: 192 drop(p=.2) no bn, state: 1024 drop(p=.3), 512 drop(p=.3), 256 drop(p=.1) -- epoch: 23, acc: 0.757
+0.561 | 0.578 | 0.603 | 0.615 | 0.632 | 0.663 | 0.67 | 0.688 | 0.696 | 0.715 | 0.729 | 0.754 | 0.762 | 0.785 | 0.801 | 0.82  | 0.84  | 0.87  | 0.908 | 0.939
 
 ---------------------------------------------------------------------------------------------
-ValueNet2 1500+, LR=3e-4, gamma=0.95, 128/16/16/16, 192, 512, 1024, 512, 256 -- epoch: 15, acc: 0.755
-ValueNet2 1500+, LR=2e-4, gamma=0.97, 128/16/16/16, 192, 512, 1024, 256 -- epoch: 11, acc: 0.749
-ValueNet2 1500+, LR=2e-4, gamma=0.97, AdamW 0.01, 128/16/16/16, 192, 512, 1024, 252, 128, 64 -- epoch: 14, acc: 0.751
-ValueNet2 1500+, LR=2e-4, gamma=0.95, AdamW 0.01, 128/16/16/16, 192, mp2d(2,2)(2,1), 512 drop, 1024, 768 -- epoch 23, acc: 0.754
+1500+, LR=2e-4, gamma=0.96, 128/16/16/16, pokemon: 192 drop(p=.2) no bn, state: 1024 drop(p=.3), 512 drop(p=.3), 256 drop(p=.1) -- epoch: 24, acc: 0.751
+1500+, LR=1e-4, gamma=0.9, 128/16/16/16, pokemon: 192 drop(p=.2) no bn, state: 1024 drop(p=.3), 512 drop(p=.3), 256 drop(p=.1) -- epoch: 20, acc: 0.748
+1500+, LR=1e-3, gamma=0.9, 128/16/16/16, pokemon: 192 drop(p=.2) no bn, state: 1024 drop(p=.3), 512 drop(p=.3), 256 drop(p=.1) -- epoch: 16, acc: 751
+1500+, LR=3e-4, gamma=0.95, 128/16/16/16, pokemon: 192 drop(p=.2) + bn, state: 1024 drop(p=.3), 512 drop(p=.3), 256 drop(p=.1) -- epoch: 18, acc: 748
+1500+, LR=3e-4, gamma=0.95, 128/16/16/16, pokemon: 192 drop(p=.2), state: 1536 drop(p=.2), 1024 drop(p=.2), 256 drop(p=.1) -- epoch: 17, acc: 0.750
 
-ValueNet2 1500+, LR=2e-4, gamma=0.98, AdamW 0.01, all move atts, 128/16/16/16, pokemon: 252, active: 64, player: 512, state: 768, 256 -- epoch: 14, acc: 0.750
-ValueNet2 1500+, LR=2e-4, gamma=0.98, AdamW 0.01, reduced atts, 128/16/16/16, pokemon: 128, active: 64, player: 512, state: 768, 256 -- epoch: 
+1250+, LR=3e-4, gamma=0.95, 128/16/16/16, pokemon: 192 drop(p=.2), state: 1536 drop(p=.2), 1024 drop(p=.2), 256 drop(p=.1) -- epoch:
 
-314 --> 128
-128 --> 64
-128 * 6 --> 512
-(512 + 64) * 2 + 21 --> 768 -> 256
-
+To try:
+    all rated data
+    larger layers
 """
 
 
 class Trainer:
-    def __init__(self,
-                 model,
-                 n_epochs=40,
-                 batch_size=252,
-                 lr=2e-4,
-                 lr_gamma=0.98,
-                 lr_decay_steps=1,
-                 num_workers=4,
-                 save_model=True
-                 ):
+    def __init__(self, model, n_epochs=50, batch_size=252, lr=3e-4, lr_gamma=0.95, lr_decay_steps=1, weight_decay=0.001, save_model=True):
 
+        # model
         self.model = model
 
         # train settings
         self.n_epochs = n_epochs
         self.batch_size = batch_size
 
-        self.optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=0.01)
+        self.optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
         self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, lr_decay_steps, gamma=lr_gamma)
+
         self.loss_function = Loss()
 
         # data settings
-        train_path = 'C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/processed-ou-incomplete/training_states/1500+/'
-        val_path = 'C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/processed-ou-incomplete/training_states/1500+validating/'
+        self.data_folder = 'C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/processed-ou-incomplete/training_states/'
+        self.file_size = 10000
 
-        shuffle_transform = StateTransformer(shuffle_players=True, shuffle_pokemon=True, shuffle_moves=True)
-        no_shuffle_transform = StateTransformer(shuffle_players=False, shuffle_pokemon=False, shuffle_moves=False)
-
-        file_size = 10000
-        last_file_size = 10000  # 7906  # 1736
-
-        self.train_samples = sum([file_size for f in os.listdir(train_path)]) - (file_size - last_file_size)
-        self.validation_samples = sum([file_size for f in os.listdir(val_path)])
-
-        self.train_loader = data_loader(train_path, shuffle_transform, batch_size=batch_size, num_workers=num_workers)
-        self.val_loader = data_loader(val_path, no_shuffle_transform, batch_size=batch_size, num_workers=num_workers)
+        self.shuffle_transform = StateTransformer(shuffle_players=True, shuffle_pokemon=True, shuffle_moves=True)
+        self.no_shuffle_transform = StateTransformer(shuffle_players=False, shuffle_pokemon=False, shuffle_moves=False)
 
         # misc. settings
+        self.num_workers = 4
         self.update_every_n_batches = 10
 
-        if save_model:
-            self.save_path = 'C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/models/training_dump/'
-        else:
-            self.save_path = None
+        self.save_model = save_model
+        self.save_path = 'C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/models/training_dump/'
 
-    def __call__(self):
-        self.batch_count = 0
+    def train(self, folder='all'):
+
+        # set data folders (options: all / 1250+ / 1500+ / 1700+)
+        train_path = os.path.join(self.data_folder, folder, 'train')
+        val_path = os.path.join(self.data_folder, folder, 'val')
+
+        n_train_samples = sum([self.file_size for f in os.listdir(train_path)])
+
+        # init data loaders
+        train_loader = data_loader(train_path, self.shuffle_transform, self.batch_size, self.num_workers)
+        val_loader = data_loader(val_path, self.no_shuffle_transform, self.batch_size, self.num_workers)
 
         # start epoch iteration
         start_time = time.time()
@@ -85,13 +76,13 @@ class Trainer:
             out_str = ''
 
             # training loop
-            epoch_train_loss = self.train()
+            epoch_train_loss = self.train_loop(train_loader, n_train_samples)
             current_lr = self.optimizer.param_groups[0]['lr']
             out_str += f"Epoch {epoch} | LR: {current_lr:.7f} | Train loss: {epoch_train_loss:.3f} | "
             print(out_str, end="")
 
             # validating loop
-            epoch_val_loss = self.validate()
+            epoch_val_loss = self.val_loop(val_loader)
             out_str += "Val loss: {:.3f} | Epoch time: {:.1f}s".format(
                 epoch_val_loss, time.time() - start_epoch_time
             )
@@ -101,23 +92,23 @@ class Trainer:
             self.scheduler.step()
 
             # save model
-            if self.save_path:
+            if self.save_model:
                 obj = {
                     "epoch": epoch,
                     "optimizer": self.optimizer.state_dict(),
                     "model": self.model.state_dict(),
                 }
-                torch.save(obj, f"{self.save_path}epoch_{epoch}.pt")
+                torch.save(obj, f"{self.save_path}{folder}_epoch_{epoch}.pt")
 
         print('Finished training')
         print("Total time: {:.1f}s".format(time.time() - start_time))
 
-    def train(self):
+    def train_loop(self, train_loader, n_train_samples):
         self.model.train()
 
         # init progress bar
         pbar = (
-            tqdm(total=self.train_samples) if self.train_samples else tqdm()
+            tqdm(total=n_train_samples) if n_train_samples else tqdm()
         )
 
         running_loss = 0.0
@@ -126,7 +117,7 @@ class Trainer:
         i = 0
 
         # iterate training batches
-        for i, sample in enumerate(self.train_loader, start=1):
+        for i, sample in enumerate(train_loader, start=1):
             self.optimizer.zero_grad()
 
             # forward pass
@@ -149,22 +140,21 @@ class Trainer:
                 pbar.set_description("Train loss: {:.3f}".format(train_loss))
 
         # complete progress bar
-        if self.train_samples:
-            pbar.update(self.train_samples - pbar.n)
+        if n_train_samples:
+            pbar.update(n_train_samples - pbar.n)
         pbar.close()
 
-        self.batch_count += i
         epoch_train_loss = epoch_train_loss / i
 
         return epoch_train_loss
 
-    def validate(self):
+    def val_loop(self, val_loader):
         self.model.eval()
         epoch_val_loss = 0.0
         i = 0
 
         # iterate batches
-        for i, sample in enumerate(self.val_loader, start=1):
+        for i, sample in enumerate(val_loader, start=1):
             with torch.no_grad():
 
                 # forward pass
