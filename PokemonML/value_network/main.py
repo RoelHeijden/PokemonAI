6f393def00bb336c8e1667e6b1a2b779c94a28d7
Roel van der Heijden <Roel.Heijden@hotmail.com>
2022-05-28 14:52:22 +0200

    notes mostly

--- PokemonML/value_network/main.py
+++ PokemonML/value_network/main.py
@@ -1,3 +1,5 @@
+import os
+
 from model.network import ValueNet
 from training import Trainer
 from testing import Tester
@@ -6,40 +8,46 @@ from testing import Tester
 """
 ---------------------- TO DO ----------------------
 
-1. set decaying lr
+1. Process more data:
+    1. Parse ALL (non-dmax) games
+    2. Split train test 90/10, sorted on folders
+    3. Create train batches: sample more states (2-6?) per game depending on length, write to multiple files?
+    4. Create test batches
 
-2. expand test module
+2. expand test module with:
+    - average eval per %complete (per label)
+    - eval distribution per %completed (per label)
+    - average eval change per %completed
+    - average accuracy per %completed (per rating)
+    - eval per turn of a single game
 
-3. find google cloud gpu details
-4. set cuda
-
-5. find right model parameters
-6. incorporate model into KetchAI
+3. find right model parameters with full dataset testing
+4. incorporate model into KetchAI
 """
 
 
 def main():
+    mode = 'train'
+    # mode = 'test'
 
-    # full input size: ((pokemon_size + pokemon_attributes_size) * 6 + side_attributes_size) * 2 + field_attributes_size
-    pokemon_size = (64 + 64 + 32 + 4 * 128) + 88
-    field_size = 21
-    side_size = 18
+    folder = '1250+'
+
+    model_name = 'epoch_17.pt'
 
     # initialize model
-    model = ValueNet(
-            field_size=field_size,
-            side_size=side_size,
-            pokemon_size=pokemon_size
-    )
+    model = ValueNet()
 
-    # # train model
-    train = Trainer(model)
-    train()
+    # train model
+    if mode == 'train':
+        trainer = Trainer(model)
+        trainer.train(folder)
 
     # test model
-    # model_file = 'epoch_30.pt'
-    # test = Tester(model, model_file)
-    # test()
+    if mode == 'test':
+        model_folder = 'C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/models/training_dump/'
+        tester = Tester(model, os.path.join(model_folder, model_name))
+        tester.test_states(folder)
+        tester.test_games(folder)
 
 
 if __name__ == '__main__':
