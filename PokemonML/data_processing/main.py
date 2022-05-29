80420eacaae4caa88266c8f6799bca2f25ba8e50
Roel van der Heijden <Roel.Heijden@hotmail.com>
2022-05-29 14:51:23 +0200

    create batches and test split folder sorting

--- PokemonML/data_processing/main.py
+++ PokemonML/data_processing/main.py
@@ -25,12 +25,12 @@ NOTEWORTHY:
 
 
 def main():
-    mode = 'parse all games'
+    # mode = 'parse all games'
     # mode = 'create test split'
-    # mode = 'create training batches'
+    mode = 'create training batches'
     # mode = 'inspect a game'
 
-    inspect_battle_id = 4421651
+    inspect_battle_id = 5685788
 
     if mode == 'parse all games':
         path_in = "C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/raw-ou-incomplete"
@@ -43,8 +43,8 @@ def main():
         create_test_split(train_path, test_path, test_split=0.10)
 
     if mode == 'create training batches':
-        path_in = 'C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/processed-ou-incomplete/test_games/all+'
-        path_out = 'C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/processed-ou-incomplete/test_states/all'
+        path_in = 'C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/processed-ou-incomplete/training_games'
+        path_out = 'C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/processed-ou-incomplete/training_states'
         f_out_name = 'ou_game_states'
         create_training_batches(path_in, path_out, f_out_name, min_rating=0, file_size=10000)
 
@@ -175,28 +175,60 @@ def get_player_ratings(game_input_log, battle_id):
 
 def create_test_split(train_path, test_path, test_split=0.10):
     """ moves files from the training folder to test folder """
+
+    # collect files
     files = [
         (
             os.path.join(train_path, file_name),
-            os.path.join(test_path, file_name)
+            file_name
         )
         for file_name in os.listdir(train_path)]
 
     print(f'{len(files)} files collected')
 
+    # output folders
+    folder_all = 'all'
+    folder_1100 = '1100+'
+    folder_1300 = '1300+'
+    folder_1500 = '1500+'
+    folder_1700 = '1700+'
+
+    # create random test split
     random.shuffle(files)
     n_test_files = int(test_split * len(files))
     test_files = files[: n_test_files]
 
     files_moved = 0
-    for f_in, f_out in test_files:
-        shutil.move(f_in, f_out)
+
+    # iterate over allocated test files
+    for f, file_name in test_files:
+
+        # get rating
+        with open(f, 'r') as f_in:
+            game_states = json.load(f_in)
+            rating = game_states[0]['average_rating']
+
+        # copy file to rating folders
+        if rating >= 1100:
+            shutil.copy(f, os.path.join(test_path, folder_1100, file_name))
+        if rating >= 1300:
+            shutil.copy(f, os.path.join(test_path, folder_1300, file_name))
+        if rating >= 1500:
+            shutil.copy(f, os.path.join(test_path, folder_1500, file_name))
+        if rating >= 1700:
+            shutil.copy(f, os.path.join(test_path, folder_1700, file_name))
+
+        # move file to the 'all' test folder
+        shutil.move(f, os.path.join(test_path, folder_all, file_name))
+
         files_moved += 1
+
         if files_moved % 10000 == 0:
             print(f'{files_moved} files moved')
 
     print(f'finished, {files_moved} files moved')
 
+    # show current file split
     train = [os.path.join(train_path, file_name)
              for file_name in os.listdir(train_path)]
 
@@ -223,18 +255,33 @@ def create_training_batches(path_in, path_out, f_out_name, min_rating=0, file_si
 
     print(f'{len(files)} files found\n')
 
-    start_time = time.time()
-
-    n_games = 0
-    n_states = 0
-    n_written = 0
-
+    # these games won't be represented in the batches
     games_too_short = {}
     ignore_list = json.load(open('games_to_ignore.json', 'r'))
 
-    # initialize first out file
-    f_out = open(os.path.join(path_out, f_out_name + '0.jsonl'), 'w')
-    n_written += 1
+    # output folders
+    folder_all = 'all'
+    folder_1100 = '1100+'
+    folder_1300 = '1300+'
+    folder_1500 = '1500+'
+    folder_1700 = '1700+'
+
+    # initialize and open first out files
+    f_out_all = open(os.path.join(path_out, folder_all,  'all_' + f_out_name + '0.jsonl'), 'w')
+    f_out_1100 = open(os.path.join(path_out, folder_1100, '1100+_' + f_out_name + '0.jsonl'), 'w')
+    f_out_1300 = open(os.path.join(path_out, folder_1300, '1300+_' + f_out_name + '0.jsonl'), 'w')
+    f_out_1500 = open(os.path.join(path_out, folder_1500, '1500+_' + f_out_name + '0.jsonl'), 'w')
+    f_out_1700 = open(os.path.join(path_out, folder_1700, '1700+_' + f_out_name + '0.jsonl'), 'w')
+
+    n_states_all = 0
+    n_states_1100 = 0
+    n_states_1300 = 0
+    n_states_1500 = 0
+    n_states_1700 = 0
+
+    n_games = 0
+
+    start_time = time.time()
 
     # open each game_states file
     for f in files:
@@ -258,41 +305,82 @@ def create_training_batches(path_in, path_out, f_out_name, min_rating=0, file_si
 
             n_games += 1
 
-            # select n random states, avoiding the team preview state
+            rating = all_states[0]['average_rating']
             n_samples = min(max(int(math.sqrt(len(all_states) * 0.82)), 1), 5)
+
+            # sample states (avoiding team preview) and write to batches
+            # all
             states = random.sample(all_states[1:], n_samples)
+            f_out_all= write_to_batch(states, f_out_all, n_states_all, file_size, f_out_name, path_out, folder_all)
+            n_states_all += len(states)
 
-            # write each extracted state to batch file
-            for s in states:
-                f_out.write(ujson.dumps(s))
-                f_out.write('\n')
+            # 1100+
+            if rating >= 1100:
+                states = random.sample(all_states[1:], n_samples)
+                f_out_1100 = write_to_batch(states, f_out_1100, n_states_1100, file_size, f_out_name, path_out, folder_1100)
+                n_states_1100 += len(states)
 
-                n_states += 1
+            # 1300+
+            if rating >= 1300:
+                states = random.sample(all_states[1:], n_samples)
+                f_out_1300 = write_to_batch(states, f_out_1300, n_states_1300, file_size, f_out_name, path_out, folder_1300)
+                n_states_1300 += len(states)
 
-                # close previous, and initialize new batch file
-                if n_states % file_size == 0:
-                    f_out.close()
-                    file = f_out_name + str(int(n_states / file_size)) + '.jsonl'
-                    f_out = open(os.path.join(path_out, file), 'w')
+            # 1500+
+            if rating >= 1500:
+                states = random.sample(all_states[1:], n_samples)
+                f_out_1500 = write_to_batch(states, f_out_1500, n_states_1500, file_size, f_out_name, path_out, folder_1500)
+                n_states_1500 += len(states)
 
-                    n_written += 1
+            # 1700+
+            if rating >= 1700:
+                states = random.sample(all_states[1:], n_samples)
+                f_out_1700 = write_to_batch(states, f_out_1700, n_states_1700, file_size, f_out_name, path_out, folder_1700)
+                n_states_1700 += len(states)
 
-                # keeping you updated
-                if n_states % 5000 == 0:
-                    print(f'{n_games} games sampled')
-                    print(f'{n_states} states extracted')
-                    print(f'{n_written} batch files created')
-                    print(f'runtime: {round(time.time() - start_time, 1)}s\n')
+            # updates
+            if n_games % 5000 == 0:
+                print(f'{n_games} games sampled')
+                print(f'states extracted: \n'
+                      f'all: {n_states_all}, \n'
+                      f'1100+: {n_states_1100}, \n'
+                      f'1300+: {n_states_1300}, \n'
+                      f'1500+: {n_states_1500}, \n'
+                      f'1700+: {n_states_1700}')
+                print(f'runtime: {round(time.time() - start_time, 1)}s\n')
 
     print(ujson.dumps(games_too_short, indent=2), '\n')
     print(f"{len(games_too_short)} games skipped because they lasted shorter than {min_game_length} turns\n")
 
     print(f'{n_games} games sampled')
-    print(f'{n_states} states extracted')
-    print(f'{n_written} batch files created with {file_size} states each')
-    print(f'final file contains {n_states % file_size} states\n')
+    print(f'states extracted: '
+          f'all {n_states_all}, '
+          f'1100+ {n_states_1100}, '
+          f'1300+ {n_states_1300}, '
+          f'1500+ {n_states_1500}, '
+          f'1700+ {n_states_1700}')
 
     print(f'runtime: {round(time.time() - start_time, 1)}s\n')
+
+
+def write_to_batch(states, f_out, n_states, file_size, next_out_name, path_out, folder):
+
+    # write each extracted state to batch file
+    for s in states:
+        f_out.write(ujson.dumps(s))
+        f_out.write('\n')
+
+        n_states += 1
+
+        # close previous, and initialize new batch file
+        if n_states % file_size == 0:
+            f_out.close()
+
+            # open new file
+            file_name = folder + '_' + next_out_name + str(int(n_states / file_size)) + '.jsonl'
+            f_out = open(os.path.join(path_out, folder, file_name), 'w')
+
+    return f_out
 
 
 def reverse_pov(state):
