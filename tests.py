3c5e44290a5fb31c4a046beacc1070858db74452
Roel van der Heijden <Roel.Heijden@hotmail.com>
2022-05-29 15:51:07 +0200

    removed empty training files

--- tests.py
+++ tests.py
@@ -12,7 +12,27 @@ import shutil
 import torch
 import torch.nn as nn
 
+"""
+C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/processed-ou-incomplete/training_games\game-states-gen8ou-4364493.json
+C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/processed-ou-incomplete/training_games\game-states-gen8ou-4369812.json
+C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/processed-ou-incomplete/training_games\game-states-gen8ou-4605078.json
+C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/processed-ou-incomplete/training_games\game-states-gen8ou-5424967.json
+"""
 
+path_in = 'C:/Users/RoelH/Documents/Uni/Bachelor thesis/data/processed-ou-incomplete/training_games'
 
+files = sorted([os.path.join(path_in, file_name)
+                for file_name in os.listdir(path_in)])
 
+print(f'{len(files)} files found')
+# 977594
+
+i = 0
+for file in files:
+    if os.stat(file).st_size == 0:
+        # os.remove(file)
+        print(file)
+        i += 1
+
+print(i)
 
