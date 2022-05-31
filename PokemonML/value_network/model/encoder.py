9a326f0ebc28be76872f7232e8ececae4f03bfde
Roel van der Heijden <Roel.Heijden@hotmail.com>
2022-05-31 15:07:57 +0200

    load custom embeddings

--- PokemonML/value_network/model/encoder.py
+++ PokemonML/value_network/model/encoder.py
@@ -1,6 +1,9 @@
 import torch
 from torch import nn
 from typing import Dict
+import fasttext
+import os
+import numpy as np
 
 from PokemonML.value_network.data.categories import (
     SPECIES,
@@ -11,13 +14,45 @@ from PokemonML.value_network.data.categories import (
 
 
 class Encoder(nn.Module):
-    def __init__(self, species_dim, move_dim, item_dim, ability_dim):
+    def __init__(self, species_dim, move_dim, item_dim, ability_dim, load_embeddings=False):
         super().__init__()
 
-        self.species_embedding = nn.Embedding(len(SPECIES) + 1, species_dim, padding_idx=0)
-        self.move_embedding = nn.Embedding(len(MOVES) + 1, move_dim, padding_idx=0)
-        self.item_embedding = nn.Embedding(len(ITEMS) + 1, item_dim, padding_idx=0)
-        self.ability_embedding = nn.Embedding(len(ABILITIES) + 1, ability_dim, padding_idx=0)
+        if not load_embeddings:
+
+            self.species_embedding = nn.Embedding(len(SPECIES) + 1, species_dim, padding_idx=0)
+            self.move_embedding = nn.Embedding(len(MOVES) + 1, move_dim, padding_idx=0)
+            self.item_embedding = nn.Embedding(len(ITEMS) + 1, item_dim, padding_idx=0)
+            self.ability_embedding = nn.Embedding(len(ABILITIES) + 1, ability_dim, padding_idx=0)
+
+        else:
+
+            species_weights = self._init_custom_embedding(species_dim, SPECIES)
+            move_weights = self._init_custom_embedding(move_dim, MOVES)
+            item_weights = self._init_custom_embedding(item_dim, ITEMS)
+            ability_weights = self._init_custom_embedding(ability_dim, ABILITIES)
+
+            self.species_embedding = nn.Embedding.from_pretrained(species_weights, freeze=True, padding_idx=0)
+            self.move_embedding = nn.Embedding.from_pretrained(move_weights, freeze=True, padding_idx=0)
+            self.item_embedding = nn.Embedding.from_pretrained(item_weights, freeze=True, padding_idx=0)
+            self.ability_embedding = nn.Embedding.from_pretrained(ability_weights, freeze=True, padding_idx=0)
+
+    def _init_custom_embedding(self, n_dims, category):
+        model_folder = 'C:/Users/RoelH/Documents//Uni/Bachelor thesis/Python/PokemonML/entity_embedding/model_files/'
+
+        # load embedding model
+        model_file = os.path.join(model_folder, 'poke_embeddings_' + str(n_dims) + '_dim.bin')
+        model = fasttext.load_model(model_file)
+
+        # concat embedding vector of each entity
+        vectors = [model.get_word_vector(x) for x in category.keys()]
+
+        # insert empty array at index 0 -- category lists start at index=1
+        vectors.insert(0, np.zeros(n_dims, dtype=float))
+
+        weights = torch.FloatTensor(vectors)
+        return weights
+
+
 
     def _concat_pokemon(self, pokemon: Dict[str, torch.tensor]) -> torch.tensor:
         """
