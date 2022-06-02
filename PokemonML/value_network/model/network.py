eaf6bcd2aac41a8ae43d627e23b181ea6358749e
Roel van der Heijden <Roel.Heijden@hotmail.com>
2022-06-02 14:08:20 +0200

    settings

--- PokemonML/value_network/model/network.py
+++ PokemonML/value_network/model/network.py
@@ -12,31 +12,26 @@ class ValueNet(nn.Module):
         # input sizes dependent on the StateTransformer output
         field_size = 21
         side_size = 18
-        pokemon_attributes = 72 + (4 * 2)
+        pokemon_attributes = 73 + (4 * 2)
 
         # encoding layer
-        species_dim = 64
-        item_dim = 16
-        ability_dim = 16
-        move_dim = 16
+        species_dim = 32
+        item_dim = 32
+        ability_dim = 32
+        move_dim = 32
         self.encoding = Encoder(species_dim, item_dim, ability_dim, move_dim, load_embeddings=True)
 
-        # input pokemon size
-        pokemon_size = (species_dim + item_dim + ability_dim + move_dim * 4) + pokemon_attributes
-
-        # reserve pokemon layer
-        reserve_out = 192
-        self.reserve_layer = PokemonLayer(pokemon_size, reserve_out, n_pokemon=10, drop_rate=0.4)
-
-        # active pokemon layer
-        active_out = 256
-        self.active_layer = PokemonLayer(pokemon_size, active_out, n_pokemon=2, drop_rate=0.1)
+        # pokemon layer
+        pokemon_in = species_dim + item_dim + ability_dim + move_dim * 4 + pokemon_attributes
+        pokemon_out = 192
+        self.pokemon_layer = PokemonLayer(pokemon_in, pokemon_out, drop_rate=0.3)
 
         # full state layer
-        state_layer_in = (reserve_out * 5 + active_out + side_size) * 2 + field_size
-        fc1_out = 1024
-        state_out = 512
-        self.state_layer = FullStateLayer(state_layer_in, fc1_out, state_out)
+        state_layer_in = pokemon_out * 12 + side_size * 2 + field_size
+        fc1_out = 2048
+        fc2_out = 512
+        state_out = 128
+        self.state_layer = FullStateLayer(state_layer_in, fc1_out, fc2_out, state_out, drop_rate1=0.5, drop_rate2=0.2)
 
         # output later
         self.output = OutputLayer(state_out)
@@ -45,17 +40,13 @@ class ValueNet(nn.Module):
         # embed and concatenate pokemon variables
         fields, sides, pokemon = self.encoding(fields, sides, pokemon)
 
-        # pass pokemon individually through the pokemon layer
-        reserve_pokemon = self.reserve_layer(pokemon[:, :, 1:])
-
-        # # pass both active pokemon together through the active pokemon layer
-        active_pokemon = self.active_layer(pokemon[:, :, :1])
+        # pass all pokemon individually through the pokemon layer
+        pokemon = self.pokemon_layer(pokemon)
 
         # pass everything together through the full state layer
         state = self.state_layer(torch.cat(
                 (
-                    reserve_pokemon,
-                    active_pokemon,
+                    torch.flatten(pokemon, start_dim=1),
                     torch.flatten(sides, start_dim=1),
                     fields
                 ),
@@ -70,11 +61,10 @@ class ValueNet(nn.Module):
 
 
 class PokemonLayer(nn.Module):
-    def __init__(self, input_size, fc1_out, n_pokemon, drop_rate):
+    def __init__(self, input_size, fc1_out, drop_rate):
         super().__init__()
 
         self.fc1 = nn.Linear(input_size, fc1_out)
-        self.bn = nn.BatchNorm1d(fc1_out * n_pokemon)
         self.drop = nn.Dropout(p=drop_rate)
         self.relu = nn.ReLU()
 
@@ -83,35 +73,41 @@ class PokemonLayer(nn.Module):
         x = self.relu(x)
         x = self.drop(x)
 
-        x = torch.flatten(x, start_dim=1)
-        x = self.bn(x)
-
         return x
 
 
 class FullStateLayer(nn.Module):
-    def __init__(self, input_size, fc1_out, output_size):
+    def __init__(self, input_size, fc1_out, fc2_out, output_size, drop_rate1, drop_rate2):
         super().__init__()
 
         self.fc1 = nn.Linear(input_size, fc1_out)
-        self.fc2 = nn.Linear(fc1_out, output_size)
+        self.fc2 = nn.Linear(fc1_out, fc2_out)
+        self.fc3 = nn.Linear(fc2_out, output_size)
 
-        self.bn1 = nn.BatchNorm1d(fc1_out)
-        self.bn2 = nn.BatchNorm1d(output_size)
+        self.bn1 = nn.BatchNorm1d(input_size)
+        self.bn2 = nn.BatchNorm1d(fc1_out)
+        self.bn3 = nn.BatchNorm1d(fc2_out)
 
-        self.drop = nn.Dropout(p=0.4)
+        self.drop1 = nn.Dropout(p=drop_rate1)
+        self.drop2 = nn.Dropout(p=drop_rate2)
+
         self.relu = nn.ReLU()
 
     def forward(self, x: torch.tensor) -> torch.tensor:
+        x = self.bn1(x)
         x = self.fc1(x)
         x = self.relu(x)
-        x = self.drop(x)
-        x = self.bn1(x)
+        x = self.drop1(x)
 
+        x = self.bn2(x)
         x = self.fc2(x)
         x = self.relu(x)
-        x = self.drop(x)
-        x = self.bn2(x)
+        x = self.drop2(x)
+
+        x = self.bn3(x)
+        x = self.fc3(x)
+        x = self.relu(x)
+        x = self.drop2(x)
 
         return x
 
@@ -120,10 +116,12 @@ class OutputLayer(nn.Module):
     def __init__(self, input_size):
         super().__init__()
 
+        self.bn = nn.BatchNorm1d(input_size)
         self.fc = nn.Linear(input_size, 1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x: torch.tensor):
+        x = self.bn(x)
         x = self.fc(x)
         x = self.sigmoid(x)
         return x
