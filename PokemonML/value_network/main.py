2f0a37faf54f97ad2aa3ceace80b77da624d2329
Roel van der Heijden <Roel.Heijden@hotmail.com>
2022-06-04 00:34:38 +0200

    notes/settings

--- PokemonML/value_network/main.py
+++ PokemonML/value_network/main.py
@@ -8,42 +8,76 @@ from model.network import ValueNet
 
 
 """
+ValueNet 1700+, 256, LR=1e-4, gamma=0.98, 64/16/16/16, pokemon/side: 192, hidden: 1024 drop(0.4), 512 drop(0.4), 128
+    Epoch 1 | Train loss: 0.708 | Val loss: 0.690
+    Epoch 2 | Train loss: 0.698 | Val loss: 0.689
+    Epoch 3 | Train loss: 0.690 | Val loss: 0.687
+    Epoch 4 | Train loss: 0.681 | Val loss: 0.674
+    Epoch 5 | Train loss: 0.664 | Val loss: 0.658 
+    Epoch 6 | Train loss: 0.638 | Val loss: 0.632
+    Epoch 7 | Train loss: 0.610 | Val loss: 0.613 
+    Epoch 8 | Train loss: 0.591 | Val loss: 0.605 
+    Epoch 9 | Train loss: 0.578 | Val loss: 0.600
+    Epoch 10 | Train loss: 0.571 | Val loss: 0.597
+    Epoch 11 | Train loss: 0.565 | Val loss: 0.599
 
-ValueNet0729 1100+, LR=2e-4, gamma=0.95, 64/16/16/16, Pokemon/side/field: 192 drop(0.1) * 12 bn, state: 1024 drop(0.3), 512 drop(0.3), 128 drop(0.1) -- epoch 30
-    21000 games evaluated
-    Sampled accuracy: 0.729
-    Overall accuracy: 0.714
-    Accuracy per %completed: 0.582 | 0.6 | 0.614 | 0.631 | 0.647 | 0.667 | 0.682 | 0.689 | 0.705 | 0.718 | 0.733 | 0.746 | 0.759 | 0.775 | 0.796 | 0.819 | 0.845 | 0.871 | 0.899 | 0.931
-                            
-ValueNet0723 1100+, LR=2e-4, gamma=0.90, 64/16/16/16, Pokemon/side/field: 192 drop(0.1) * 12 bn, state: 1024 drop(0.3), 512 drop(0.3), 128 drop(0.1) -- epoch 12
-    14900 games evaluated
-    Sampled accuracy: 0.723
-    Overall accuracy: 0.713
-    Accuracy per %completed: 0.576 | 0.597 | 0.615 | 0.629 | 0.635 | 0.654 | 0.674 | 0.685 | 0.702 | 0.716 | 0.73 | 0.746 | 0.763 | 0.782 | 0.803 | 0.823 | 0.845 | 0.873 | 0.905 | 0.931
-
-ValueNet 1100+, batch size 256, LR=2e-4, gamma=0.98, 64/16train/16train/16train, pokemon: 192 drop(0.4), hidden: 1024 drop(0.4), 512 drop(0.4) -- epoch 14:
-test states:
-    all: 0.727
-    1100+: 0.719
-    1300+: 0.712
-    1500+: 0.707
-    1700+: 0.719*
+ValueNet 1700+, 256, LR=1e-4, gamma=0.98, 64/64/64/64, pokemon/side: 192, hidden: 1024 drop(0.4), 512 drop(0.4), 128
+    Epoch 1 | Train loss: 0.713 | Val loss: 0.695
+    Epoch 2 | Train loss: 0.700 | Val loss: 0.693
+    Epoch 3 | Train loss: 0.695 | Val loss: 0.689
+    Epoch 4 | Train loss: 0.687 | Val loss: 0.680
+    Epoch 5 | Train loss: 0.673 | Val loss: 0.662
+    Epoch 6 | Train loss: 0.649 | Val loss: 0.640
+    Epoch 7 | Train loss: 0.624 | Val loss: 0.620
+    Epoch 8 | Train loss: 0.598 | Val loss: 0.610
+    Epoch 9 | Train loss: 0.578 | Val loss: 0.605
+    Epoch 10 | Train loss: 0.566 | Val loss: 0.600 
+    Epoch 11 | Train loss: 0.557 | Val loss: 0.594
+    Epoch 12 | Train loss: 0.549 | Val loss: 0.593
+    Epoch 13 | Train loss: 0.544 | Val loss: 0.594
+    
+ValueNet 1700+, 256, LR=1e-4, gamma=0.98, 64/16/16/16, matchup 4, pokemon/side: 192, hidden: 1024 drop(0.4), 512 drop(0.4), 128
+    Epoch 13 | Train loss: 0.555 | Val loss: 0.582
 
 
-ValueNet 1100+, batch size 256, LR=15e-5, gamma=0.96, 32/32/32train/32train, pokemon: 192 drop(0.3), hidden: 2048 drop(0.5), 512 drop(0.2), 128 drop(0.2)
+ValueNet 1500+, 256, LR=2e-4, gamma=0.95, 64/16/16/16, matchup 5, pokemon/side: 192, hidden: 1024 drop(0.4), 512 drop(0.4), 128
+Baseline:
+    Epoch 1 | Train loss: 0.664 | Val loss: 0.596
+    Epoch 2 | Train loss: 0.579 | Val loss: 0.577
+    Epoch 3 | Train loss: 0.566 | Val loss: 0.572 
+    Epoch 4 | Train loss: 0.561 | Val loss: 0.570
+    Epoch 5 | Train loss: 0.558 | Val loss: 0.568 
+    Epoch 6 | Train loss: 0.555 | Val loss: 0.567
+    Epoch 7 | Train loss: 0.552 | Val loss: 0.568
+    Epoch 8 | Train loss: 0.549 | Val loss: 0.566 
+    Epoch 9 | Train loss: 0.546 | Val loss: 0.564
+    Epoch 10 | Train loss: 0.543 | Val loss: 0.564 
+    Epoch 11 | Train loss: 0.541 | Val loss: 0.563 X
+    Epoch 12 | Train loss: 0.538 | Val loss: 0.566 
+    Epoch 13 | Train loss: 0.536 | Val loss: 0.564
+    Epoch 14 | Train loss: 0.533 | Val loss: 0.566 
 
 
-try cyclic LR??
+2d pokemon batchnorm, 2d matchup batchnorm, pokemon_out -> matchup_layer
+2d pokemon batchnorm, 2d matchup batchnorm, pokemon_in -> matchup_layer
+1d pokemon batchnorm, 2d matchup batchnorm, ...
 
 
+
+
+try:
+    embedding sizes
+    drop rates
+    convolution
+    
 """
 
 
 def main():
     model = ValueNet()
-    rating = '1100+'
+    rating = '1500+'
     folder = 'training_dump'
-    model_name = '1100+_epoch_14'
+    model_name = '1700+_epoch_13'
 
     train(model, rating, folder, model_name, train_new=True)
     # train(model, rating, folder, model_name, train_new=False)
@@ -60,8 +94,8 @@ def train(model, rating, folder, model_name, train_new=True):
 
     n_epochs = 50
     batch_size = 256
-    lr = 15e-5
-    lr_decay = 0.96
+    lr = 2e-4
+    lr_decay = 0.95
     lr_decay_steps = 1
 
     trainer = Trainer(
@@ -73,7 +107,15 @@ def train(model, rating, folder, model_name, train_new=True):
         lr=lr,
         lr_decay=lr_decay,
         lr_decay_steps=lr_decay_steps,
-        update_every_n_batches=50
+        update_every_n_batches=20,
+        file_size=10000,
+        buffer_size=5000,
+        num_workers=4,
+        shuffle_data=True,
+        shuffle_players=True,
+        shuffle_pokemon=True,
+        shuffle_moves=True,
+        save_model=True
     )
 
     if train_new:
